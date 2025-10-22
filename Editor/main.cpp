0773b0b93b809b2061981b5ef744e0a7961e4cc7
chineseYouth <revolution202781@outlook.com>
2025-10-23 00:11:54 +0800

    1

--- Editor/main.cpp
+++ Editor/main.cpp
@@ -10,7 +10,7 @@ using namespace RE::Editor;
 int main(int argc, char* argv[]) {
   QApplication app(argc, argv);
 
-  app.setApplicationName("Revolution-Editor");
+  app.setApplicationName("REditor");
   app.setApplicationVersion("1.0");
   app.setOrganizationName("Revolution");
 
