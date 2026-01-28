78bf6213fa5f82bef2a433a6192573fabe4567af
chineseYouth <revolution202781@outlook.com>
2026-01-28 20:43:57 +0800

    1

--- Engine/Render/RHI/pch.h
+++ Engine/Render/RHI/pch.h
@@ -13,7 +13,7 @@
 
 #pragma once
 
-#include "Engine/Core/Core.h"
+#include "Engine/Core/Base.h"
 
 #pragma warning(disable : 4201)  // nonstandard extension used : nameless struct/union
 #pragma warning(disable : 4238)  // nonstandard extension used : class rvalue used as lvalue
